22f6f3c3b842d30b5086137455be4b53eb7b74a4
Eduardo Corpeño <kuashio@gmail.com>
2023-03-23 00:18:51 +0000

    updated MLP.h

--- src/03_02b/MLP.h
+++ src/03_02b/MLP.h
@@ -7,31 +7,29 @@
 #include <cmath>
 #include <time.h>
 
-using namespace std;
-
 class Perceptron {
 	public: 
-		vector<double> weights;
+		std::vector<double> weights;
 		double bias;
 		Perceptron(int inputs, double bias=1.0);
-        double run(vector<double> x);
-		void set_weights(vector<double> w_init);
+        double run(std::vector<double> x);
+		void set_weights(std::vector<double> w_init);
 		double sigmoid(double x);
 };
 
 class MultiLayerPerceptron {
 	public: 
-		MultiLayerPerceptron(vector<int> layers, double bias=1.0, double eta = 0.5);
-		void set_weights(vector<vector<vector<double> > > w_init);
+		MultiLayerPerceptron(std::vector<int> layers, double bias=1.0, double eta = 0.5);
+		void set_weights(std::vector<std::vector<std::vector<double> > > w_init);
 		void print_weights();
-		vector<double> run(vector<double> x);
-		double bp(vector<double> x, vector<double> y);
+		std::vector<double> run(std::vector<double> x);
+		double bp(std::vector<double> x, std::vector<double> y);
 		
-		vector<int> layers;
+		std::vector<int> layers;
 		double bias;
 		double eta;
-		vector<vector<Perceptron> > network;
-		vector<vector<double> > values;
-		vector<vector<double> > d;
+		std::vector<std::vector<Perceptron> > network;
+		std::vector<std::vector<double> > values;
+		std::vector<std::vector<double> > d;
 };
 
